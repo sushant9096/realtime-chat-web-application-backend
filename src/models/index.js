ebd30528449a654e511d535f9de6c5b85613e828
Sushant Zope <sushantzope9096@gmail.com>
2025-02-01 20:30:12 +0530

    improve error message in auth middleware

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,5 @@
-const {Sequelize} = require("sequelize");
-const {db} = require("../config");
+const { Sequelize } = require("sequelize");
+const { db } = require("../config");
 
 const sequelize = new Sequelize(db.NAME, db.USER, db.PASS, {
   host: db.HOST,
@@ -8,39 +8,47 @@ const sequelize = new Sequelize(db.NAME, db.USER, db.PASS, {
 });
 
 const modelDefiners = [
-  require('./conversation.model'),
-  require('./message.model'),
-  require('./user.model'),
-  require('./participant.model')
+  require("./conversation.model"),
+  require("./message.model"),
+  require("./user.model"),
+  require("./participant.model"),
 ];
 
 for (let modelDefiner of modelDefiners) {
-  modelDefiner(sequelize)
+  modelDefiner(sequelize);
 }
 
-const {conversation, message, participant, user} = sequelize.models;
+const { conversation, message, participant, user } = sequelize.models;
 
-user.hasMany(participant, {foreignKey: 'userId'});
-participant.belongsTo(user, {foreignKey: 'userId'});
-conversation.hasMany(participant, {foreignKey: 'conversationId'});
-conversation.hasMany(message, {foreignKey: 'conversationId'});
-conversation.belongsToMany(user, {through: participant, foreignKey: 'conversationId'});
-user.belongsToMany(conversation, {through: participant, foreignKey: 'userId'});
-participant.belongsTo(conversation, {foreignKey: 'conversationId'});
+user.hasMany(participant, { foreignKey: "userId" });
+participant.belongsTo(user, { foreignKey: "userId" });
+conversation.hasMany(participant, { foreignKey: "conversationId" });
+conversation.hasMany(message, { foreignKey: "conversationId" });
+conversation.belongsToMany(user, {
+  through: participant,
+  foreignKey: "conversationId",
+});
+user.belongsToMany(conversation, {
+  through: participant,
+  foreignKey: "userId",
+});
+participant.belongsTo(conversation, { foreignKey: "conversationId" });
 
 message.belongsTo(conversation);
 
-sequelize.sync({
-  // alter: true,
-  // force: true,
-}).then(r => {
-  console.log('Database & tables synced!');
-})
+sequelize
+  .sync({
+    // alter: true,
+    // force: true,
+  })
+  .then((r) => {
+    console.log("Database & tables synced!");
+  });
 
 module.exports = {
   conversationModel: conversation,
   messageModel: message,
   participantModel: participant,
   userModel: user,
-  sequelize
+  sequelize,
 };
